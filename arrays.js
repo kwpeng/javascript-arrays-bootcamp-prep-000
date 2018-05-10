3f77b7465248ff911adf878454b1a5ea243766df
bluebeadlily@gmail.com <bluebeadlily@gmail.com>
2018-05-10 04:59:43 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -35,18 +35,23 @@ return array[index];
 
 function destructivelyRemoveElementFromBeginningOfArray(array){
   array.shift();
-  return array
+  return array;
   }
   
   function removeElementFromBeginningOfArray(array){
   array = array.slice(1);
-  return array
+  return array;
   }
   
   console.log(removeElementFromBeginningOfArray([1,2,3,4]));
   
   
-  destructivelyRemoveElementFromEndOfArray
+ function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop();
+  return array;
+   }
+   
+   
   
   
   
